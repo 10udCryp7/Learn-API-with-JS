2252993655db43cf138508840cc8aaef0f311b1c
Đỗ Trí Dũng <22025501@vnu.edu.vn>
2023-11-07 00:21:47 +0700

    first video code about fetch, async, await, promise, handle error

---
+++ Part 1/1_Fetch.js
@@ -0,0 +1,7 @@
+// 1. call fetch (______) __: path of file -> both retrieve and send
+// 2. response
+// 3. complete data stream -> grap data from response: text, blob (image), json -> for data
+// 4. make an <img> element with data
+
+// need learn async promise
+
